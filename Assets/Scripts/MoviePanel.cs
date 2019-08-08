5ddee3ccce17f3da6afc525719fa329ada934781
Patrick <patrick@battleverse.com>
2019-08-07 21:51:27 -0400

    Assets Commit

---
+++ Assets/Scripts/MoviePanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class MoviePanel : MonoBehaviour
+{
+
+    [SerializeField]
+    Text title;
+    [SerializeField]
+    Image imagePoster;
+
+    public void SetTitle(string _title)
+    {
+        title.text = _title;
+    }
+
+    public void SetImagePoster(string _textureURL)
+    {
+        StartCoroutine(downloadMoviePoster(_textureURL));
+    }
+
+    IEnumerator downloadMoviePoster(string _textureURL)
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(_textureURL);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        }
+        else
+        {
+            Texture downloadedTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            imagePoster.sprite = Sprite.Create(downloadedTexture as Texture2D, new Rect(0, 0, downloadedTexture.width, downloadedTexture.height), new Vector2(0, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/MovieSearchManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct SearchResults
+{
+    [System.Serializable]
+    public struct MovieContents
+    {
+        public string Title;
+        public string Year;
+        public string imdbID;
+        public string Type;
+        public string Poster;
+    }
+
+    public List<MovieContents> Search;
+}
+
+public class MovieSearchManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject emptyMoviePanel;
+    [SerializeField]
+    Transform contentPanel;
+
+    List<MoviePanel> moviePanels;
+
+    InputField _inputField;
+
+    //http://www.omdbapi.com/?apikey=6367da97&s=sun
+
+    private void Awake()
+    {
+        moviePanels = new List<MoviePanel>();
+    }
+
+    private void Start()
+    {
+        _inputField = GetComponent<InputField>();
+    }
+
+    public void DoSearch()
+    {
+        DoSearch("http://www.omdbapi.com/?apikey=6367da97&s=" + _inputField.text);
+    }
+
+    public void DoSearch(string _searchURL)
+    {
+        StartCoroutine(DownloadJsonSearchResult(_searchURL));
+    }
+
+    IEnumerator DownloadJsonSearchResult(string _searchURL)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(_searchURL);
+        yield return www.SendWebRequest();
+
+        if (www.isHttpError || www.isNetworkError)
+        {
+            Debug.LogError(www.error);
+        }
+        else
+        {
+            ClearMoviePanels();
+            CreateMoviePanels(JsonUtility.FromJson<SearchResults>(www.downloadHandler.text));
+        }
+    }
+
+    void CreateMoviePanels(SearchResults _movieList)
+    {
+        foreach(SearchResults.MovieContents _movieContent in _movieList.Search)
+        {
+            GameObject _newMovieListing = Instantiate(emptyMoviePanel, contentPanel);
+            MoviePanel _newMoviePanel = _newMovieListing.GetComponent<MoviePanel>();
+
+            _newMoviePanel.SetTitle(_movieContent.Title);
+            _newMoviePanel.SetImagePoster(_movieContent.Poster);
+
+            moviePanels.Add(_newMoviePanel);
+
+        }
+    }
+
+    void ClearMoviePanels()
+    {
+        while (moviePanels.Count > 0)
+        {
+            Destroy(moviePanels[0]);
+        }
+
+        moviePanels.Clear();
+    }
+}
